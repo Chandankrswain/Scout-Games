f91a783b4d3f1bda55210f46f031f49a1c321008
Chandan Kumar Swain <mr.chandanswain@gmail.com>
2024-02-24 00:18:33 +0530

    Removed Chakra and Installed Tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,7 @@
-import { Grid, GridItem, Show } from "@chakra-ui/react";
-
 function App() {
   return (
     <>
-      <Grid
-        templateAreas={{
-          base: `"nav" "main"`,
-          lg: `"nav nav" "aside main"`, // screen size wider than 1024px
-        }}
-      >
-        <GridItem area="nav" bg="lightgreen">
-          Nav
-        </GridItem>
-        <Show above="lg">
-          <GridItem area="aside" bg="lightblue">
-            Aside
-          </GridItem>
-        </Show>
-        <GridItem area="main" bg="lightpink">
-          Main
-        </GridItem>
-      </Grid>
+      <h1 className="">Helloworld</h1>
     </>
   );
 }
