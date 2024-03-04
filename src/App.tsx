895d1fe06e0fa4063440bd82d375a31f8149e3fb
Chandan Kumar Swain <mr.chandanswain@gmail.com>
2024-03-04 21:42:44 +0530

    Fetch the Games

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import Navbar from "./components/Navbar";
 import { Grid, GridItem, Show } from "@chakra-ui/react";
+import GameGrid from "./components/GameGrid";
 
 function App() {
   return (
@@ -17,7 +18,9 @@ function App() {
           <GridItem area="aside">Aside</GridItem>
         </Show>
 
-        <GridItem area="main">Main</GridItem>
+        <GridItem area="main">
+          <GameGrid />
+        </GridItem>
       </Grid>
     </>
   );
