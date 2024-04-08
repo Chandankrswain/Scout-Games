1d3597537e159cc5a2b7368446569408c50bd673
Chandan Kumar Swain <mr.chandanswain@gmail.com>
2024-04-09 02:35:53 +0530

    Modified the Search Bar component

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,39 +1,19 @@
-import { useState, useEffect } from "react";
-import searchLight from "../images/searchWhite.png";
-import searchBlack from "../images/searchBlack.png";
-import {
-  InputGroup,
-  InputLeftElement,
-  Input,
-  useColorMode,
-} from "@chakra-ui/react";
+import { InputGroup, InputLeftElement, Input } from "@chakra-ui/react";
 import { BsSearch } from "react-icons/bs";
 
 const SearchBar = () => {
-  const { colorMode, toggleColorMode } = useColorMode();
-
-  const [imageSrc, setImageSrc] = useState(
-    colorMode === "dark" ? searchLight : searchBlack
-  );
-
-  useEffect(() => {
-    setImageSrc(colorMode === "dark" ? searchLight : searchBlack);
-  }, [colorMode]);
-
-  const handleToggle = () => {
-    toggleColorMode();
-  };
-
   return (
-    <InputGroup>
-      <InputLeftElement children={<BsSearch />}></InputLeftElement>
-      <Input
-        type="text"
-        placeholder="Search games..."
-        size="md"
-        borderRadius="80px"
-      />
-    </InputGroup>
+    <form onSubmit={(event) => event.preventDefault()}>
+      <InputGroup>
+        <InputLeftElement children={<BsSearch />}></InputLeftElement>
+        <Input
+          type="text"
+          placeholder="Search games..."
+          size="md"
+          borderRadius="80px"
+        />
+      </InputGroup>
+    </form>
   );
 };
 
